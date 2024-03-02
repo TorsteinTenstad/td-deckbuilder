efe9712bb9a9be795d73249cdf78b5e16c71492d
Torstein Tenstad <torstein.tenstad@norsonic.com>
2024-03-02 22:32:10 +0100

    Implement BuffAura::update

--- src/common/component_buff_aura.rs
+++ src/common/component_buff_aura.rs
@@ -1,6 +1,10 @@
 use serde::{Deserialize, Serialize};
 
-use crate::{buff::Buff, update_args::UpdateArgs};
+use crate::{
+    buff::{buff_add_to_entity, Buff},
+    entity::Entity,
+    update_args::UpdateArgs,
+};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct BuffAura {
@@ -9,5 +13,23 @@ pub struct BuffAura {
 }
 
 impl BuffAura {
-    pub fn update(_update_args: &mut UpdateArgs) {}
+    pub fn update(update_args: &mut UpdateArgs) {
+        let pos = update_args.entity.pos;
+        for buff_aura in update_args.entity.buff_auras.iter() {
+            let in_range = |entity: &&mut Entity| {
+                !buff_aura
+                    .radius
+                    .is_some_and(|r| (entity.pos - pos).length_squared() > r * r)
+            };
+
+            for entity in update_args
+                .dynamic_game_state
+                .entities
+                .iter_mut()
+                .filter(in_range)
+            {
+                buff_add_to_entity(entity, buff_aura.buff.clone())
+            }
+        }
+    }
 }
