b8e6fcdc6c97e91a03e0bc8c93969dacb0b6d2d0
Torstein Tenstad <torstein.tenstad@norsonic.com>
2024-03-26 16:25:20 +0100

    Restrict interface of Entities

--- src/common/entities.rs
+++ src/common/entities.rs
@@ -1,60 +1,100 @@
 use serde::{Deserialize, Serialize};
 
-use crate::entity::EntityInstance;
+use crate::{
+    entity::{EntityInstance, EntityState},
+    game_loop::{cleanup_entity, update_entity},
+    game_state::ServerControlledGameState,
+    update_args::UpdateArgs,
+};
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct Entities {
-    entities: Vec<EntityInstance>,
-}
+pub struct Entities(Vec<EntityInstance>);
 
 impl std::ops::Deref for Entities {
     type Target = [EntityInstance];
 
     fn deref(&self) -> &Self::Target {
-        &self.entities
+        &self.0
     }
 }
 
 impl std::ops::DerefMut for Entities {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.entities
+        &mut self.0
     }
 }
 
 impl Entities {
-    pub fn len(&self) -> usize {
-        self.entities.len()
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.entities.is_empty()
-    }
-
     pub fn at(&self, index: usize) -> Option<&EntityInstance> {
-        self.entities.get(index)
+        self.0.get(index)
     }
 
     pub fn at_mut(&mut self, index: usize) -> Option<&mut EntityInstance> {
-        self.entities.get_mut(index)
+        self.0.get_mut(index)
     }
 
     pub fn iter(&self) -> std::slice::Iter<'_, EntityInstance> {
-        self.entities.iter()
+        self.0.iter()
     }
 
     pub fn iter_mut(&mut self) -> std::slice::IterMut<'_, EntityInstance> {
-        self.entities.iter_mut()
-    }
-
-    pub fn swap_remove(&mut self, index: usize) -> EntityInstance {
-        self.entities.swap_remove(index)
-    }
-
-    pub fn insert(&mut self, index: usize, entity: EntityInstance) {
-        self.entities.insert(index, entity);
+        self.0.iter_mut()
     }
 
     pub fn push(&mut self, entity: EntityInstance) {
-        self.entities.push(entity);
+        self.0.push(entity);
+    }
+}
+
+pub fn update_entities(server_controlled_game_state: &mut ServerControlledGameState, dt: f32) {
+    let mut i = server_controlled_game_state
+        .dynamic_game_state
+        .entities
+        .0
+        .len()
+        - 1;
+    while i > 0 {
+        let mut entity_instance = server_controlled_game_state
+            .dynamic_game_state
+            .entities
+            .0
+            .swap_remove(i);
+        update_entity(&mut UpdateArgs {
+            static_game_state: &server_controlled_game_state.static_game_state,
+            semi_static_game_state: &mut server_controlled_game_state.semi_static_game_state,
+            dynamic_game_state: &mut server_controlled_game_state.dynamic_game_state,
+            entity_instance: &mut entity_instance,
+            dt,
+        });
+        server_controlled_game_state
+            .dynamic_game_state
+            .entities
+            .push(entity_instance);
+        i -= 1;
+    }
+}
+
+pub fn remove_dead_entities(server_controlled_game_state: &mut ServerControlledGameState) {
+    let mut i = 0;
+    while i < server_controlled_game_state
+        .dynamic_game_state
+        .entities
+        .len()
+    {
+        let entity = &server_controlled_game_state
+            .dynamic_game_state
+            .entities
+            .get(i)
+            .unwrap();
+        if entity.state == EntityState::Dead {
+            cleanup_entity(entity.id, server_controlled_game_state);
+            server_controlled_game_state
+                .dynamic_game_state
+                .entities
+                .0
+                .swap_remove(i);
+        } else {
+            i += 1;
+        }
     }
 }
--- src/common/game_loop.rs
+++ src/common/game_loop.rs
@@ -1,12 +1,20 @@
 use crate::{
-    buff::buff_update_timers, component_attack::Attack, component_buff_source::BuffSource,
-    component_health::Health, component_movement::Movement, config::CLOSE_ENOUGH_TO_TARGET,
-    entity::EntityState, find_target::find_target_for_attack,
-    game_state::ServerControlledGameState, ids::EntityId, update_args::UpdateArgs,
+    buff::buff_update_timers,
+    component_attack::Attack,
+    component_buff_source::BuffSource,
+    component_health::Health,
+    component_movement::Movement,
+    config::CLOSE_ENOUGH_TO_TARGET,
+    entities::{remove_dead_entities, update_entities},
+    entity::EntityState,
+    find_target::find_target_for_attack,
+    game_state::ServerControlledGameState,
+    ids::EntityId,
+    update_args::UpdateArgs,
     world::world_place_building,
 };
 
-fn cleanup_entity(
+pub fn cleanup_entity(
     entity_id: EntityId,
     server_controlled_game_state: &mut ServerControlledGameState,
 ) {
@@ -17,59 +25,6 @@ fn cleanup_entity(
         .find(|(_id, building_location)| building_location.entity_id == Some(entity_id))
     {
         building_location.entity_id = None;
-    }
-}
-
-fn remove_dead_entities(server_controlled_game_state: &mut ServerControlledGameState) {
-    let mut i = 0;
-    while i < server_controlled_game_state
-        .dynamic_game_state
-        .entities
-        .len()
-    {
-        let entity = &server_controlled_game_state
-            .dynamic_game_state
-            .entities
-            .get(i)
-            .unwrap();
-        if entity.state == EntityState::Dead {
-            cleanup_entity(entity.id, server_controlled_game_state);
-            server_controlled_game_state
-                .dynamic_game_state
-                .entities
-                .swap_remove(i);
-        } else {
-            i += 1;
-        }
-    }
-}
-
-fn update_entities(server_controlled_game_state: &mut ServerControlledGameState, dt: f32) {
-    // Implementation may cause entities to not be updated if the update_entities directly removes entities.
-    // Killing entities should only be done by setting their state to dead.
-    let mut i = 0;
-    while i < server_controlled_game_state
-        .dynamic_game_state
-        .entities
-        .len()
-    {
-        let mut entity_instance = server_controlled_game_state
-            .dynamic_game_state
-            .entities
-            .swap_remove(i);
-        update_entity(&mut UpdateArgs {
-            static_game_state: &server_controlled_game_state.static_game_state,
-            semi_static_game_state: &mut server_controlled_game_state.semi_static_game_state,
-            dynamic_game_state: &mut server_controlled_game_state.dynamic_game_state,
-            entity_instance: &mut entity_instance,
-            dt,
-        });
-        // TODO: Inserting at i causes a lot of memory movement, this can be optimized using a better swap routine for updating.
-        server_controlled_game_state
-            .dynamic_game_state
-            .entities
-            .insert(i, entity_instance);
-        i += 1;
     }
 }
 
